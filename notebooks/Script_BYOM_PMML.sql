5546e9a02e9892c500921c173aec9f71628a16f8
Luis C <lcajachahua@gmail.com>
2023-09-27 07:55:19 -0600

    Update Script_BYOM_PMML.sql

--- notebooks/Script_BYOM_PMML.sql
+++ notebooks/Script_BYOM_PMML.sql
@@ -1,4 +1,4 @@
-DATABASE DATALAB; --- Reemplace con el nombre de su BD
+DATABASE demo_user; --- Reemplace con el nombre de su BD
 
 --- CONSTRUIMOS LA TABLA DONDE SE ALMACENAN LOS MODELOS
 
@@ -20,11 +20,12 @@ select * from pmml_models;
 
 --- REVISAMOS LA TABLA DE SCORING Y EJECUTAMOS PMMLPredict
 
-select * from new_churn;
+--- select top 10 * from new_churn;
 
-SELECT * FROM TRNG_BYOM.PMMLPredict (
+SELECT * FROM mldb.PMMLPredict (
     ON new_churn AS InputTable
     ON (SELECT * FROM pmml_models WHERE model_id='churn_model') AS ModelTable DIMENSION
     USING
       Accumulate ('Phone')
+      ModelOutputFields ('probability(0)', 'probability(1)') 
 ) AS td;
